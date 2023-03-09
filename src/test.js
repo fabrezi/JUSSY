827b8a46be3a4b193f3abe3fc2b0730c9a51a197
fabrezi <fkschon@gmail.com>
2023-03-09 12:25:48 -0500

    test001

--- src/test.js
+++ src/test.js
@@ -1,1 +1,10 @@
-console.log("hola");
+/*
+*the task is to make a random number generator.
+*the length will be 4 digits.
+*/
+
+set = Math.random();
+
+for( i=0 ; i < 1000 ; i++){
+    console.log(i, 'NO BLOCKERS');
+}
