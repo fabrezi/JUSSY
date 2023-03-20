efd0e3378be27a8ddd4616d5645b222610f7eb33
fabrezi <fkschon@gmail.com>
2023-03-20 12:11:32 -0400

    new stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # JUSSY
 writing JS spaghetti code.
+
+### JS
+
+The name JavaScript is redundant and confusing. I call it Nodes. 
+Nodes has become imperative for modern Mobile/Web design.
+
+
---
+++ src/temp_conversion.js
@@ -0,0 +1,11 @@
+const farenheit = 65
+
+let celsius = ((farenheit -  32) * 5) / 9
+
+/*
+* the JS has two string concatenation 
+  the comma concatenates string with 1 or some
+  space in middle. + sign is to join the two strings
+*/
+
+console.log("the temperature today is:" ,celsius + "C");
