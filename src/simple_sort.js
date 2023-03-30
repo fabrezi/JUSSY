8fcbcc05a0dbe2c00c844a1a90058d70edba06ca
fabrezi <fkschon@gmail.com>
2023-03-30 17:42:05 -0400

    burrr

--- src/simple_sort.js
+++ src/simple_sort.js
@@ -1,61 +1,15 @@
 //sort: 5,7,-1,0,2
 //output: -1, 0 , 2, 5, 7
 
-/*
-print an array of random nombers
-sort alg.
+//case01: use with sort arr:
 
-~ most of the numeric alg problems 
-  can be solved with the following
-  equation:
+//the use of built in methods
+//make it easy to implement.
+//But the logic is not explained.
 
-  class(X);
+const arr = [5,7,-1,0,2];
+sorts = arr.sort();
 
-  main(X);
-
-  Interestingly, the same method 
-  is not usually applied with JS.
-  It uses the `exports.modules` 
-  method but it doesn't follow
-  the class method.
-
-sample-code:
-
-arr = [5,7,0];
-
-while(arr > arr.length){
-    for()
-}
-
-
-
-I have also seen the Array.isArray(). 
-
-It is a method to deduce whether the passed parameter is 
-an array or not.
-
-arr.sort() is an in-built method to perform sort.
-*/
-
-const arr = [5,7,0];
-
-//arr.sort()
-
-// while(arr < arr.length){
-//     for(i=0; i< arr.length; i++){
-//         if(arr[i] > arr[i+1]){
-//             arr[i] == arr[i+1];
-//             arr[i+1] == arr[i];
-//         }
-//         return arr;
-//     }
-// }
-
-console.log(arr.sort());
-
-// while(arr < arr.length){
-//     console.log("get out");
-// }
-
-return 0;
-
+console.log("sorts" + sorts);
+ports = arr.pop();
+console.log("pop" + ports);
