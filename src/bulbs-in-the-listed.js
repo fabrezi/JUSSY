42f83e3cd96f3e5e11d92b2a61e20fbbbd08e5bf
fabrezi <fkschon@gmail.com>
2023-03-30 17:30:54 -0400

    file002

---
+++ src/bulbs-in-the-listed.js
@@ -0,0 +1,16 @@
+/*
+*the input will be an array. the following 
+is a sample thou shall be followed:
+
+int array[] = [x,o,x,o]
+
+as it be processed to be:
+
+if x then no change
+if o then change
+
+outcome: [x,x,x,x]
+*/
+
+
+
--- src/simple_sort.js
+++ src/simple_sort.js
@@ -5,17 +5,57 @@
 print an array of random nombers
 sort alg.
 
-check(the first index){
-    if(num == 0){
-        res.message: "recheck it"
-    }else {
-        
-    }
+~ most of the numeric alg problems 
+  can be solved with the following
+  equation:
+
+  class(X);
+
+  main(X);
+
+  Interestingly, the same method 
+  is not usually applied with JS.
+  It uses the `exports.modules` 
+  method but it doesn't follow
+  the class method.
+
+sample-code:
+
+arr = [5,7,0];
+
+while(arr > arr.length){
+    for()
 }
+
+
+
+I have also seen the Array.isArray(). 
+
+It is a method to deduce whether the passed parameter is 
+an array or not.
+
+arr.sort() is an in-built method to perform sort.
 */
 
-module.exports = class Sorter {
-    int A [] = [1,34,2];
-    
+const arr = [5,7,0];
 
-}
+//arr.sort()
+
+// while(arr < arr.length){
+//     for(i=0; i< arr.length; i++){
+//         if(arr[i] > arr[i+1]){
+//             arr[i] == arr[i+1];
+//             arr[i+1] == arr[i];
+//         }
+//         return arr;
+//     }
+// }
+
+console.log(arr.sort());
+
+// while(arr < arr.length){
+//     console.log("get out");
+// }
+
+return 0;
+
